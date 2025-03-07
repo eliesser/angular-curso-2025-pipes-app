f9deda69626032e5d50e32aea62a32be08ab674b
Eliesser Freites <freiteseliesser@hotmail.com>
2025-03-07 21:11:38 +0100

    add initial page components and configure routing with Tailwind CSS integration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'basic',
+    loadComponent: () => import('./pages/basic-page/basic-page.component'),
+  },
+  {
+    path: 'numbers',
+    loadComponent: () => import('./pages/numbers-page/numbers-page.component'),
+  },
+  {
+    path: 'uncommon',
+    loadComponent: () =>
+      import('./pages/uncommon-page/uncommon-page.component'),
+  },
+  {
+    path: 'custom',
+    loadComponent: () => import('./pages/custom-page/custom-page.component'),
+  },
+  {
+    path: '**',
+    redirectTo: 'basic',
+  },
+];
