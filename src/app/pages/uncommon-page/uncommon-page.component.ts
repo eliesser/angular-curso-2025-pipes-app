6a63dc3a48659f571f7aa34a8ddf57b2645dbe6f
Eliesser Freites <freiteseliesser@hotmail.com>
2025-03-07 22:34:31 +0100

    enhance uncommon page with dynamic client invitation and gender-based greeting

--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
+import { I18nSelectPipe } from '@angular/common';
+
+const client1 = {
+  name: 'Eliesser',
+  gender: 'male',
+  age: 37,
+  address: 'Spain, Ourense',
+};
+
+const client2 = {
+  name: 'Gissel',
+  gender: 'female',
+  age: 35,
+  address: 'Spain, Ourense',
+};
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [CardComponent],
+  imports: [CardComponent, I18nSelectPipe],
   templateUrl: './uncommon-page.component.html',
 })
-export default class UncommonPageComponent {}
+export default class UncommonPageComponent {
+  client = signal(client1);
+
+  invitationMap = {
+    male: 'Mr.',
+    female: 'Mrs.',
+  };
+
+  changeClient() {
+    if (this.client() === client1) {
+      this.client.set(client2);
+    } else {
+      this.client.set(client1);
+    }
+  }
+}
